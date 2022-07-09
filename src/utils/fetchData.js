91b3e016ccffd8273408c5633920bf4750b73e0b
shahriar-tamjid <shahriar.tamjid@outlook.com>
2022-07-09 21:09:39 +0600

    Fetching Data from Rapid API

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const options = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": "d6204f9201msh35c9b9a7917c9eep135052jsnaf6a1d8f407c",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
